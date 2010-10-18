56e4b5e1c1fe0643807e8cbdfa63f5c9d42ed5ba
HowardvanRooijen <howardvanrooijen@gmail.com>
2010-10-18 12:41:43 +0100

    Add Hosting.AspNet content

---
+++ Solutions/OpenRasta.Hosting.AspNet/Iis6.cs
@@ -0,0 +1,26 @@
+namespace OpenRasta.Hosting.AspNet
+{
+    using System.Collections.Generic;
+    using System.Web.Configuration;
+
+    public class Iis6 : Iis
+    {
+        public override IEnumerable<HttpHandlerRegistration> Handlers
+        {
+            get
+            {
+                var httpHandlers = (HttpHandlersSection)WebConfigurationManager.GetSection("system.web/httpHandlers");
+
+                foreach (HttpHandlerAction section in httpHandlers.Handlers)
+                {
+                    var handler = new HttpHandlerRegistration(section.Verb, section.Path, section.Type);
+
+                    if (IsHandlerRegistrationValid(handler))
+                    {
+                        yield return handler;
+                    }
+                }
+            }
+        }
+    }
+}
